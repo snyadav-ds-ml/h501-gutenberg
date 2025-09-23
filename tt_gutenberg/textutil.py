b6e719270aed065f2195e94c3f603d384b3c05a8
Sneha Yadav <snyadav@iu.edu>
2025-09-23 00:54:58 -0400

    Update textutil.py
    comments

--- tt_gutenberg/textutil.py
+++ tt_gutenberg/textutil.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def read_csv_d():
+    """
+    This function simply reads author and language csv and returns the dataset 
+    I have just used this fucntion to understand how modules can call each other within same package
+    """
     gutenberg_authors = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv')
     gutenberg_languages = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv')
     return gutenberg_authors, gutenberg_languages
