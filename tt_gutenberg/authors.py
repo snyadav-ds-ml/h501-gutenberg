43e5c01faee2a935eb2ffb756f863fee27678596
Sneha <snyadav@iu.edu>
2025-09-23 00:11:59 -0400

    fixed func input

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from textutil import read_csv_d
+from .textutil import read_csv_d
 
 def list_authors(by_languages, alias):
     
     df_a,df_l = read_csv_d()
 
-    print(df_l['total_languages'].nunique)
+    #print(df_l['total_languages'].nunique)
     merged_df = merged_df = pd.merge(
         df_a,
         df_l,
@@ -21,7 +21,7 @@ def list_authors(by_languages, alias):
 
     if by_languages and alias:
         out=merged_df.groupby('alias')['total_languages'].sum().reset_index()
-        print(out.head(10))
+        #print(out.head(10))
         alias_trans_count_sorted = out.sort_values(by='total_languages', ascending=False)
         auth_list = alias_trans_count_sorted['alias'].tolist()
     
--- tt_gutenberg/textutil.py
+++ tt_gutenberg/textutil.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def read_csv_d():
     gutenberg_authors = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv')
     gutenberg_languages = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv')
