37ad69d563c1a1a9453b15c9ddbc55f5ba985592
Sneha Yadav <snyadav@iu.edu>
2025-10-06 23:33:02 +0000

    metadata df for merge

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,26 +1,35 @@
 import pandas as pd
 from .textutil import read_csv_d
 
+
 def list_authors(by_languages, alias):
     """
     This function takes boolen input and returns alias list w.r.t. tranaslation count, otherwise just returns the authors list.
     """
     
-    df_a,df_l = read_csv_d()
+    df_a, df_l, df_m = read_csv_d()
     
-    # merges two datasets
-    merged_df = merged_df = pd.merge(
-        df_a,
-        df_l,
-        left_on='gutenberg_author_id',    
-        right_on='gutenberg_id', 
+    
+    # merges two datasets on gutenberg_id
+    merged_df_l_m = pd.merge(
+        df_m,
+        df_l,   
+        on='gutenberg_id', 
         how='inner'           
     )
 
-    merged_df = merged_df.dropna(subset=['alias'])
-    #print(merged_df.head(10))
-    #print(list(merged_df.columns))
+    #print(merged_df_l_m.columns)
 
+    merged_df = pd.merge(
+        merged_df_l_m,
+        df_a,   
+        on='gutenberg_author_id',
+        how='inner'           
+    )
+
+    #print(merged_df.columns)
+
+    merged_df['alias'].dropna().drop_duplicates()
 
     if by_languages and alias:
         out=merged_df.groupby('alias')['total_languages'].sum().reset_index()
@@ -30,9 +39,8 @@ def list_authors(by_languages, alias):
     
     else:
         out = df_a['author'].dropna().drop_duplicates()
-        auth_list=df_a['author'].tolist()
+        auth_list=out.tolist()
     
     
 
     return auth_list
-
--- tt_gutenberg/textutil.py
+++ tt_gutenberg/textutil.py
@@ -7,4 +7,5 @@ def read_csv_d():
     """
     gutenberg_authors = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv')
     gutenberg_languages = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_languages.csv')
-    return gutenberg_authors, gutenberg_languages
+    gutenberg_metadata = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_metadata.csv')
+    return gutenberg_authors, gutenberg_languages, gutenberg_metadata
